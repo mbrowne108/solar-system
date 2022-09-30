8573ac51550c3e1128134399488a8294596cc7e5
Matt <mbrowne108@gmail.com>
2022-09-30 14:48:42 -0400

    Add aiming line and hover logic

--- solar-system.js
+++ solar-system.js
@@ -12,6 +12,7 @@ sun.style.left = `${sunX}px`
 sun.style.top = `${sunY}px`
 
 const mercury = {
+    name: 'Mercury',
     speed: 0.004787,
     theta: Math.random() * 2 * Math.PI,
     radius: 80,
@@ -19,6 +20,7 @@ const mercury = {
 }
 
 const venus = {
+    name: 'Venus',
     speed: -(0.003502),
     theta: Math.random() * 2 * Math.PI,
     radius: 110,
@@ -26,6 +28,7 @@ const venus = {
 }
 
 const earth = {
+    name: 'Earth',
     speed: 0.002978,
     theta: Math.random() * 2 * Math.PI,
     radius: 150,
@@ -34,6 +37,7 @@ const earth = {
 }
 
 const mars = {
+    name: 'Mars',
     speed: 0.0024077,
     theta: Math.random() * 2 * Math.PI,
     radius: 180,
@@ -41,6 +45,7 @@ const mars = {
 }
 
 const jupiter = {
+    name: 'Jupiter',
     speed: 0.001307,
     theta: Math.random() * 2 * Math.PI,
     radius: 235,
@@ -48,6 +53,7 @@ const jupiter = {
 }
 
 const saturn = {
+    name: 'Saturn',
     speed: 0.000969,
     theta: Math.random() * 2 * Math.PI,
     radius: 280,
@@ -55,6 +61,7 @@ const saturn = {
 }
 
 const uranus = {
+    name: 'Uranus',
     speed: -(0.000681),
     theta: Math.random() * 2 * Math.PI,
     radius: 330,
@@ -62,6 +69,7 @@ const uranus = {
 }
 
 const neptune = {
+    name: 'Neptune',
     speed: 0.000543,
     theta: Math.random() * 2 * Math.PI,
     radius: 385,
@@ -69,6 +77,7 @@ const neptune = {
 }
 
 const pluto = {
+    name: 'Pluto',
     speed: 0.000474,
     theta: Math.random() * 2 * Math.PI,
     radius: 450,
@@ -76,6 +85,7 @@ const pluto = {
 }
 
 const moon = {
+    name: 'Moon',
     speed: 0.002978 * 12,
     theta: Math.random() * 2 * Math.PI,
     radius: 15,
@@ -95,25 +105,41 @@ const planets = [
 ]
 
 function update(planet) {
-    ctx.beginPath();
-    ctx.moveTo(parseInt(planet.el.style.left), parseInt(planet.el.style.top))
+    // ctx.beginPath();
+    // ctx.moveTo(parseInt(planet.el.style.left), parseInt(planet.el.style.top))
 
+    planet.el.addEventListener('mouseenter', () => {
+        ctx.clearRect(0, 0, 100, 30)
+        ctx.font = "18px Arial"
+        ctx.fillStyle = "white"
+        ctx.fillText(planet.name, 10, 10)
+    })
+    
     planet.theta -= planet.speed
     planet.el.style.left = `${Math.cos(planet.theta) * planet.radius + sunX + 50}px`
     planet.el.style.top = `${Math.sin(planet.theta) * planet.radius + sunY + 50}px`
 
-    ctx.lineTo(parseInt(planet.el.style.left), parseInt(planet.el.style.top))
-    ctx.strokeStyle = "#ffffff"
-    ctx.stroke()
+    // ctx.lineTo(parseInt(planet.el.style.left), parseInt(planet.el.style.top))
+    // ctx.strokeStyle = "#ffffff"
+    // ctx.stroke()
 
     if (planet.hasMoon) {
         moon.theta -= moon.speed
         moon.el.style.left = `${Math.cos(moon.theta) * moon.radius + parseInt(planet.el.style.left)}px`
         moon.el.style.top = `${Math.sin(moon.theta) * moon.radius + parseInt(planet.el.style.top)}px`
     }
+}
 
+function aimLine(e) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.strokeStyle = "blue"
+    ctx.beginPath()
+    ctx.moveTo(parseInt(earth.el.style.left), parseInt(earth.el.style.top))
+    ctx.lineTo(e.clientX, e.clientY)
+    ctx.stroke()
 }
 
 setInterval(() => {
-    planets.forEach(update)
+    canvas.addEventListener("mousemove", aimLine)
+    planets.forEach(update)  
 }, 1000 / FPS)
